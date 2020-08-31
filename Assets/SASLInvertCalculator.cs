e5cc4b3b1bf5d30ca6227ae4b2d6f09a2c397d7d
Robert Kasper <firestorm1550@gmail.com>
2020-08-30 20:51:27 -0400

    Progress..?

---
+++ Assets/SASLInvertCalculator.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using DAS_Unity_Framework.ExtensionMethods;
+using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+
+public class SASLInvertCalculator : MonoBehaviour
+{
+    public Text output;
+    
+    public float shoulderToTorsoAngleDegrees;
+    public float torsoToLegsAngleDegrees;
+
+    public float torsoMassKg = 55f;
+    public float torsoLengthMeters = .6f;
+    public float torsoCenterOfGravity = .4f;
+    
+    public float legsMassKg = 35f;
+    public float legsLengthMeters = .9f;
+    public float legsCenterOfGravity = .4f;
+
+
+    private void Update()
+    {
+        output.text = GetTorqueOnShoulder().RoundToNearest(.01f) + " KN*M";
+    }
+
+
+    public float GetTorqueOnShoulder()
+    {
+        return GetTorqueOnShoulderFromLegs() + GetTorqueOnShoulderFromTorso();
+    }
+    
+    private float GetTorqueOnShoulderFromTorso()
+    {
+        float shouldersToHipsX = GetDistanceShouldersToHipsInX();
+        return torsoMassKg * shouldersToHipsX * torsoCenterOfGravity;
+    }
+
+    private float GetDistanceShouldersToHipsInX()
+    {
+        float portionOfLengthInX = Mathf.Sin(shoulderToTorsoAngleDegrees * Mathf.Deg2Rad);
+        return torsoLengthMeters * portionOfLengthInX;
+    }
+    
+    private float GetTorqueOnShoulderFromLegs()
+    {
+        float xDistanceHipsToLegCoG =
+            Mathf.Sin((torsoToLegsAngleDegrees - shoulderToTorsoAngleDegrees) * Mathf.Deg2Rad) * legsCenterOfGravity;
+        float xDistanceShouldersToLegCoG = GetDistanceShouldersToHipsInX() + xDistanceHipsToLegCoG;
+            
+        return legsLengthMeters * xDistanceShouldersToLegCoG;
+    }
+}
