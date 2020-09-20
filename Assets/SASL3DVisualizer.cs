f3cc90ba46ebd783c049aa5e0fcb7b83b67b4ee4
Robert Kasper <firestorm1550@gmail.com>
2020-09-20 16:55:04 -0400

    Properly modifies Ethan shoulder and hips

---
+++ Assets/SASL3DVisualizer.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using DAS_Unity_Framework.ExtensionMethods;
+using UnityEngine;
+using UnityEngine.Animations;
+
+public class SASL3DVisualizer : MonoBehaviour
+{
+    public SASLInvertCalculator data;
+
+    public Transform skeletonRoot;
+    
+    public Transform rightShoulderJoint;
+    public Transform leftShoulderJoint;
+
+    public Transform rightHipJoint;
+    public Transform leftHipJoint;
+
+    public Transform rightHand;
+    public Transform leftHand;
+    
+    
+    private Dictionary<Transform, Quaternion> startRotations;
+    private void Start()
+    {
+        startRotations = new Dictionary<Transform, Quaternion>();
+        startRotations.Add(rightShoulderJoint, rightShoulderJoint.localRotation);
+        startRotations.Add(leftShoulderJoint, leftShoulderJoint.localRotation);
+        startRotations.Add(rightHipJoint, rightHipJoint.localRotation);
+        startRotations.Add(leftHipJoint, leftHipJoint.localRotation);
+        startRotations.Add(skeletonRoot, skeletonRoot.localRotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ApplySASLData();
+    }
+
+    private void ApplySASLData()
+    {
+        Vector3 shoulderAxis = rightShoulderJoint.position - leftShoulderJoint.position;
+        shoulderAxis.Normalize();
+
+        Vector3 hipAxis = rightHipJoint.position - leftHipJoint.position;
+        hipAxis.Normalize();
+
+        foreach (Transform joint in startRotations.Keys)
+        {
+            joint.localRotation = startRotations[joint];
+        }
+        
+        float theta = 180 - data.shoulderToTorsoAngleDegrees;
+        rightShoulderJoint.RotateAround(rightShoulderJoint.position, shoulderAxis, theta);
+        leftShoulderJoint.RotateAround(rightShoulderJoint.position, shoulderAxis, theta);
+
+
+        float alpha = 180 - data.torsoToLegsAngleDegrees;
+        rightHipJoint.Rotate(hipAxis, alpha);
+        leftHipJoint.Rotate(hipAxis, alpha);
+
+    }
+}
---
+++ Assets/SASLVisualization.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SASLInvertCalculator))]
+public class SASLVisualization : MonoBehaviour
+{
+
+
+    public Material pointColor;
+    public Material lineColor;
+    public Vector3 startPoint;
+    
+    private SASLInvertCalculator data;
+    
+    private LineRenderer shoulderJoint;
+    private LineRenderer torso;
+    private LineRenderer hipJoint;
+    private LineRenderer legs;
+
+    private bool initialized;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(initialized)
+            DrawVisualization();
+    }
+
+
+    private void Awake()
+    {
+        data = GetComponent<SASLInvertCalculator>();
+        
+        shoulderJoint = InitializeJoint("Shoulders");
+        torso = InitializeLine("Torso");
+        hipJoint = InitializeJoint("Hips");
+        legs = InitializeLine("Legs"); 
+        initialized = true;
+    }
+
+    private LineRenderer InitializeJoint(string jointName)
+    {
+        GameObject jointObj = new GameObject(jointName);
+        jointObj.transform.parent = transform;
+        LineRenderer joint = jointObj.AddComponent<LineRenderer>();
+        joint.material = pointColor;
+        joint.numCapVertices = 12;
+        joint.widthMultiplier = .15f;
+        return joint;
+    }
+    
+    private LineRenderer InitializeLine(string lineName)
+    {
+        GameObject lineObj = new GameObject(lineName);
+        lineObj.transform.parent = transform;
+        LineRenderer line = lineObj.AddComponent<LineRenderer>();
+        line.material = lineColor;
+        line.widthMultiplier = .05f;
+        return line;
+    }
+    
+    private void DrawVisualization()
+    {
+        shoulderJoint.SetPositions(new []
+        {
+            startPoint, startPoint
+        });
+
+        Vector3 hipJointPos = GetHipJointPosition();
+        
+        torso.SetPositions(new []{startPoint, hipJointPos});
+        hipJoint.SetPositions(new[]{hipJointPos,hipJointPos});
+        legs.SetPositions(new[]{hipJointPos, Vector3.zero, });
+    }
+
+    private Vector3 GetHipJointPosition()
+    {
+        float torsoLength = data.torsoLengthMeters;
+        float shoulderAngle = data.shoulderToTorsoAngleDegrees;
+
+        float theta = 90 - shoulderAngle;
+        
+        float x = torsoLength * Mathf.Sin(Mathf.Deg2Rad * shoulderAngle);
+        float y = torsoLength * Mathf.Cos(Mathf.Deg2Rad * shoulderAngle);
+        
+        return startPoint + new Vector3(x,y,0);
+    }
+
+    private Vector3 GetToePosition(Vector3 hipJointPosition)
+    {
+        return new Vector3();
+    }
+}
